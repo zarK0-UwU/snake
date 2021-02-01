80ffe87bac79cb6f28bcb6f57f3d66a0b6483d7a
zark0 <josezarco222@gmail.com>
2021-02-01 11:45:22 +0100

    Update mainGUI.java

--- GoCLS/mainGUI.java
+++ GoCLS/mainGUI.java
@@ -1,5 +1,11 @@
-public interface MainGUI {
+import javax.swing.JFrame;
 
+public class MainGUI {
+
+    public MainGUI(){
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
     public static void main (String[] args){
         
     }
